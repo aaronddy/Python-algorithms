404d4939f1bac23d18237781ce3d0eeda1eef4d3
aaronddy <aaronddy94@gmail.com>
2021-02-19 16:25:42 +0900

    basic algorithms of characters

--- part_3_sequnce_control_structure/14_1_handling_characters.py
+++ part_3_sequnce_control_structure/14_1_handling_characters.py
@@ -1,1 +1,40 @@
-# test
+# 14.2-1  자릿수값의 총합 계산하기
+
+number = input("세 자리 정수를 입력하라: ")
+print("넘버의 데이터 타입은? ", type(number))   # str
+
+a, b, c = number
+result = int(a) + int(b) + int(c)
+
+print("자릿수값의 합: ", result)
+
+
+
+# 14.3-1  문자열을 역순으로 출력하기
+
+# code1
+cha = input('네 개의 문자를 가진 문자열을 입력하라: ')
+
+l1 = cha[0]
+l2 = cha[1]
+l3 = cha[2]
+l4 = cha[3]
+
+letters = l4+l3+l2+l1
+
+print("code1_letters:", letters)
+
+
+# code2
+
+m, n, o, p = cha
+
+letters2 = p+o+n+m
+
+print("code2_letters:", letters2)
+
+
+# code3
+
+letters3 = cha[::-1]
+print("code3_letters:",letters3)
